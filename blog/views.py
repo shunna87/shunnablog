8f9d79a3257845cc6c75f160fddd9d1107dd30c7
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2020-07-23 22:32:22 +0200

    update blog views

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 def blog(request):
-    return render(request, 'blog/blog.html')
+    articles = Article.objects.all()
+    data = {'articles': articles}
+    return render(request, 'blog/blog.html', data)
+
+def detail(request, article_id):
+    article_data = Article.objects.all().filter(id=article_id)
+    data = {'article_details': article_data}
+    return render(request, 'blog/article.html', data)
