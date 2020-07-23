cd48521f0953bdb9f244f12dfb3c04e573503b72
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2020-07-23 22:28:05 +0200

    create article model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Article(models.Model):
+    """Model definition for Article."""
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="article")
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/', default='images/default.png')
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Meta definition for Article."""
+
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+
+    def __str__(self):
+        """Unicode representation of Article."""
+        pass
